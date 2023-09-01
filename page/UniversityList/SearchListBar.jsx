c6e3d63b5394928346cda4634565c374dc29d74e
c0c0pang <coc1789@naver.com>
2023-09-01 15:39:50 +0900

    feat: 검색창 기능 구현
    아직 검색 시 해당하는 대학교는 안나옴

---
+++ page/UniversityList/SearchListBar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
+import { UniDataSet } from '../../data/Data';
+import { Dimensions } from 'react-native';
+import useCustomFont from '../../data/useCustomFont';
+function SearchListBar() {
+    const [selectedItem, setSelectedItem] = useState(null);
+    const fontLoaded = useCustomFont('Bazzi', require('../../assets/fonts/Bazzi.ttf'))
+    return (
+        <AutocompleteDropdown
+            onSelectItem={setSelectedItem}
+            dataSet={UniDataSet}
+            clearOnFocus={false}
+            closeOnBlur={true}
+            containerStyle={{ flexGrow: 1, flexShrink: 1 }}
+            suggestionsListMaxHeight={Dimensions.get('window').height * 0.2}
+            textInputProps={{
+                placeholder: '찾고자 하는 학교를 입력하세요.',
+                placeholderTextColor: '#fff',
+                autoCorrect: false,
+                autoCapitalize: 'none',
+                style: {
+                    borderRadius: 3,
+                    // backgroundColor: '#F6A79C',
+                    color: '#000',
+                    paddingLeft: 18,
+                    fontFamily: fontLoaded ? 'Bazzi' : {}
+                },
+
+            }}
+            inputContainerStyle={{
+                backgroundColor: '#F6A79C',
+                borderRadius: 3,
+            }}
+            suggestionsListContainerStyle={{
+                backgroundColor: '#F6A79C',
+            }}
+            suggestionsListTextStyle={{
+                fontFamily: fontLoaded ? 'Bazzi' : {},
+                color: '#000'
+            }}
+            showChevron={false}
+        />
+    )
+}
+
+export default SearchListBar
