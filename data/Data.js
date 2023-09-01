52be80db8cb669366b2b4f80a122aaec3d826343
c0c0pang <coc1789@naver.com>
2023-09-01 15:41:35 +0900

    feat: 더미 데이터 추가

---
+++ data/Data.js
@@ -0,0 +1,116 @@
+import Ad1 from '../assets/img/Ad1.svg'
+import Ad2 from '../assets/img/Ad2.svg'
+import Ad3 from '../assets/img/Ad3.svg'
+import Ad4 from '../assets/img/Ad4.svg'
+import Catholic from '../assets/img/Catholic.svg'
+import Konkuk from '../assets/img/Konkuk.svg'
+import Kyonggi from '../assets/img/Kyonggi.svg'
+import KyungHee from '../assets/img/KyungHee.svg'
+import Korea from '../assets/img/Korea.svg'
+import Kwangwoon from '../assets/img/Kwangwoon.svg'
+import Kookmin from '../assets/img/Kookmin.svg'
+const userInfo = [
+    {
+        id: 0,
+        name: '최원석',
+        point: 4000
+    }
+]
+export const RankData = [
+    {
+        id: 0,
+        name: '최원석',
+        uni: '광운대'
+    },
+    {
+        id: 1,
+        name: '송종찬',
+        uni: '성균관대'
+    },
+    {
+        id: 2,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 3,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 4,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 5,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 6,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 7,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 8,
+        name: '제로',
+        uni: '제로대학교'
+    },
+    {
+        id: 9,
+        name: '제로',
+        uni: '제로대학교'
+    }
+]
+export const AdList = [
+    Ad1, Ad2, Ad3, Ad4
+]
+export const UniList = [
+    Catholic,
+    Konkuk,
+    Kyonggi,
+    KyungHee
+]
+export const UniDataSet = [
+    {
+        id: '1',
+        title: '가톨릭대',
+        uni: Catholic
+    },
+    {
+        id: '2',
+        title: '건국대',
+        uni: Konkuk
+    },
+    {
+        id: '3',
+        title: '경기대',
+        uni: Kyonggi
+    },
+    {
+        id: '4',
+        title: '경희대',
+        uni: KyungHee
+    }
+    , {
+        id: '5',
+        title: '국민대',
+        uni: Kookmin
+    },
+    {
+        id: '6',
+        title: '광운대',
+        uni: Kwangwoon
+    },
+    {
+        id: '7',
+        title: '고려대',
+        uni: Korea
+    }
+]
