6c43ef073df2c74f7a31a7eff6ac0de9c30b5a07
c0c0pang <coc1789@naver.com>
2023-09-01 15:41:06 +0900

    feat: 기존에 하단 네비게이션을 컴포넌트화하고 폰트를 위한 로딩 기능 추가

--- App.js
+++ App.js
@@ -1,45 +1,25 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { WithLocalSvg } from 'react-native-svg';//svg사용에 필수
-import HomePage from './page/home/HomePage';
-import MapPage from './page/Map/MapPage';
-import MorePage from './page/More/MorePage';
-import HomeIcon from './/assets/icon/Home.svg';
-import HomeCIcon from './assets/icon/HomeC.svg';
-import MapIcon from './assets/icon/Map.svg';
-import MapCIcon from './assets/icon/MapC.svg';
-import MoreIcon from './assets/icon/More.svg';
-import MoreCIcon from './assets/icon/MoreC.svg';
+import React, { useEffect, useState } from 'react';
+import BottomNav from './navigation/BottomNav';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 export default function App() {
-  const Tab = createBottomTabNavigator();
+  const [isReady, setIsReady] = useState(false);
+  const getFonts = async () => {
+    await Font.loadAsync({
+      Bazzi: require("./assets/fonts/Bazzi.ttf"),
+    });
+    setIsReady(true)
+  };
+  useEffect(() => {
+    getFonts()
+  }, [])
   return (
-    <NavigationContainer>
+    isReady ? (<BottomNav></BottomNav>) : (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setIsReady(true)}
+        onError={() => { }}
+      />)
 
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          unmountOnBlur: true,
-          tabBarIcon: ({ focused }) => {
-            if (route.name === "Home") {
-              return <WithLocalSvg asset={focused ? (HomeCIcon) : (HomeIcon)} />;
-            }
-            if (route.name === "Map") {
-              return <WithLocalSvg asset={focused ? (MapCIcon) : (MapIcon)} />;
-            }
-            if (route.name === "More") {
-              return <WithLocalSvg asset={focused ? (MoreCIcon) : (MoreIcon)} />;
-            }
-          },
-          tabBarShowLabel: false,
-          headerShown: false,
-          headerShadowVisible: false, // 헤더 border 제거
-        })}
-      >
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Map" component={MapPage} />
-        <Tab.Screen name="More" component={MorePage} />
-      </Tab.Navigator>
-    </NavigationContainer >
   );
 }
