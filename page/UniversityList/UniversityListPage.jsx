ba694077df02c50acb89520dcf8270e11c004817
c0c0pang <coc1789@naver.com>
2023-09-01 15:40:11 +0900

    feat: 개인 및 대학교 랭킹 View 제작
    더미 데이터로 구현

---
+++ page/UniversityList/UniversityListPage.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import { AutocompleteDropdown, AutocompleteDropdownContextProvider } from 'react-native-autocomplete-dropdown';
+import { UniDataSet } from '../../data/Data';
+import { Dimensions, View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { SearchBackView, UniFalatList, UniImgCardView, UniImgText, UniLyoutView, UniSearchView } from '../../style/universityStyled/universityStyledComponent';
+import SearchListBar from './SearchListBar';
+import { WithLocalSvg } from 'react-native-svg';
+import { ThemeProvider } from 'styled-components/native';
+import useCustomFont from '../../data/useCustomFont';
+import BackIcon from '../../assets/icon/Back.svg'
+import { useNavigation } from '@react-navigation/native';
+import { GlobaSafeAreaView } from '../../style/globalStyled';
+function UniversityListPage() {
+    const fontLoaded = useCustomFont('Bazzi', require('../../assets/fonts/Bazzi.ttf'));
+    const navigation = useNavigation();
+    return (
+        <AutocompleteDropdownContextProvider>
+            <GlobaSafeAreaView>
+                <UniSearchView>
+                    <SearchBackView>
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <WithLocalSvg asset={BackIcon} width={32} height={32} />
+                        </TouchableOpacity>
+                        <SearchListBar />
+                    </SearchBackView>
+                    <ThemeProvider theme={fontLoaded ? { fontFamily: 'Bazzi' } : {}}>
+                        <UniFalatList
+                            data={UniDataSet}
+                            renderItem={({ item }) => (
+                                <UniImgCardView>
+                                    <WithLocalSvg asset={item.uni} width={53} height={53} />
+                                    <UniImgText>{item.title}</UniImgText>
+                                </UniImgCardView>
+                            )}
+                            keyExtractor={(item, index) => index}
+                            numColumns={3}
+                            columnWrapperStyle={{
+                                justifyContent: 'space-between',
+                                marginBottom: 22
+                            }}
+                        />
+                    </ThemeProvider>
+                </UniSearchView>
+            </GlobaSafeAreaView>
+        </AutocompleteDropdownContextProvider>
+    )
+}
+
+export default UniversityListPage
