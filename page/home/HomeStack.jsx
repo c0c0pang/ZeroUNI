1f20c26569f64f07800e9180e056d4810fb024c4
c0c0pang <coc1789@naver.com>
2023-10-05 00:21:57 +0900

    feat: 카메라 화면의 따른 상태 변경 기능 구현

--- page/home/HomeStack.jsx
+++ page/home/HomeStack.jsx
@@ -3,22 +3,43 @@ import React from 'react';
 import UniversityListPage from '../UniversityList/UniversityListPage';
 import HomeScreen from './HomeScreen';
 import CameraScreen from '../Camera/CameraScreen';
+import { useSetRecoilState } from 'recoil';
+import { navigatorState } from '../../atom/homeAtom';
+import UniverMarketPage from '../UniversityList/UniverMarketPage';
 
 const Stack = createNativeStackNavigator();
 
 function HomeStack() {
+    const setShowTabBar = useSetRecoilState(navigatorState)
     return (
         <Stack.Navigator
             initialRouteName='Home'
             screenOptions={({ route }) => ({
-
                 headerShown: false
             })
             }
+
         >
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="UniListPage" component={UniversityListPage} />
-            <Stack.Screen name="Camera" component={CameraScreen} />
+            <Stack.Group>
+                <Stack.Screen name="UniListPage" component={UniversityListPage} />
+                <Stack.Screen name="UniMarketPage" component={UniverMarketPage} />
+            </Stack.Group>
+            <Stack.Screen
+                name="Camera"
+                component={CameraScreen}
+                listeners={({ route }) => ({
+                    focus: () => {
+                        setShowTabBar(true);
+                    },
+                    blur: () => {
+                        setShowTabBar(true);
+                    },
+                })}
+                options={{
+                    gestureEnabled: true
+                }}
+            />
         </Stack.Navigator>
     )
 }
