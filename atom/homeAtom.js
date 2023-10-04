c83217f68e62f38b3938dbff081bb410caa157eb
c0c0pang <coc1789@naver.com>
2023-10-05 00:23:55 +0900

    feat: 전역 상태 관리를 위한 atom 생성
    바텀 네비게이션 상태와 아이템 상태 제작

---
+++ atom/homeAtom.js
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const navigatorState = atom({
+    key: 'navigatorState',
+    default: false
+})
+export const univerItem = atom({
+    key: 'univerItemState',
+    default: null
+})
