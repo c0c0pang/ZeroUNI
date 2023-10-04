b0a0d2c7005e71fefc362bce85702583b9fd9e9c
c0c0pang <coc1789@naver.com>
2023-10-05 00:20:59 +0900

    feat: 카메라 버튼 기능 구현

--- page/Camera/CameraButton.jsx
+++ page/Camera/CameraButton.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import CameraIcon from '../../assets/icon/CameraBut.svg'
 import { WithLocalSvg } from 'react-native-svg'
-import { CameraButtonView } from '../../style/cameraStyled/cameraStyledComponent'
-function CameraButton() {
+import { AfterText, AfterView, CameraButtonTouchableOpacity } from '../../style/cameraStyled/cameraStyledComponent'
+import { Text, View } from 'react-native'
+import { BazziText } from '../../style/homeStyled/homeStyledComponent'
+import useCustomFont from '../../data/useCustomFont'
+import { ThemeProvider } from 'styled-components/native'
+function CameraButton(props) {
+    const fontLoaded = async () => await useCustomFont('Bazzi', require('../../assets/fonts/Bazzi.ttf'));
     return (
-        <CameraButtonView>
-            <WithLocalSvg asset={CameraIcon} />
-        </CameraButtonView>
+        <ThemeProvider theme={fontLoaded ? { fontFamily: 'Bazzi' } : {}}>
+            <CameraButtonTouchableOpacity onPress={props.onPress}>
+                {props.icon ? (
+                    <View style={{ padding: 50 }}>
+                        <WithLocalSvg asset={props.icon} />
+                    </View>
+
+                ) : (
+                    <AfterView color={props.color}>
+                        <AfterText>{props.text}</AfterText>
+                    </AfterView>
+                )}
+            </CameraButtonTouchableOpacity>
+        </ThemeProvider>
     )
 }
 
