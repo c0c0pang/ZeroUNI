3839f2bb953171272a1ad82c68b496821cd1e6fd
c0c0pang <coc1789@naver.com>
2023-10-29 03:07:32 +0900

    feat: 폰트 불러오기 스플래시 이용

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import BottomNav from './navigation/BottomNav';
 import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import BottomNav from './navigation/BottomNav';
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const getFonts = async () => {
+  // 앱의 초기화 또는 다른 준비 작업을 수행합니다.
+  const initializeApp = async () => {
     await Font.loadAsync({
       Bazzi: require("./assets/fonts/Bazzi.ttf"),
     });
-    setIsReady(true)
-  };
-  useEffect(() => {
-    getFonts()
-  }, [])
-  return (
-    isReady ? (<BottomNav></BottomNav>) : (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setIsReady(true)}
-        onError={() => { }}
-      />)
 
+    // 앱 초기화가 완료되면 스플래시 스크린을 숨깁니다.
+    SplashScreen.hideAsync();
+    setIsReady(true);
+  };
+
+  useEffect(() => {
+    // 스플래시 스크린을 자동으로 숨기기 위해 preventAutoHideAsync()를 호출합니다.
+    SplashScreen.preventAutoHideAsync();
+
+    // 앱 초기화 함수를 호출합니다.
+    initializeApp();
+  }, []);
+
+  return (
+    <RecoilRoot>
+      {isReady ? <BottomNav /> : null}
+    </RecoilRoot>
   );
 }
